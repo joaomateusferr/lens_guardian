bc218582d2ab4053b7075be17a326194966f005e
João Ferreira <joaomateusferr@gmail.com>
2019-08-25 17:25:41 -0300

    IOT login + BTN reset

--- inicial.py
+++ inicial.py
@@ -1,16 +1,28 @@
 import os
 
-print("Preencha os dados da sua rede WIFI")
+print("Fill in the  WIFI data!")
 
 ssid = raw_input('SSID: ')
-senha = raw_input('Senha: ')
+wifi_password = raw_input('Password: ')
 
 #network = open("/etc/wpa_supplicant/wpa_supplicant.conf, "w+") raspberypi
 network = open("wpa_supplicant.conf", "w+")
-network.write('ctrl_interface=DIR=/var/run/wpa_supplicant GROUP = netdev\nupdate_config = 1\n\nnetwork = {\n\tssid = "%s"\n\tpsk = "%s"\n}' % (ssid,senha))
+network.write('ctrl_interface=DIR=/var/run/wpa_supplicant GROUP = netdev\nupdate_config = 1\n\nnetwork = {\n\tssid = "%s"\n\tpsk = "%s"\n}' % (ssid,wifi_password))
 network.close()
 
-#login no vinos aqui
+url_login = 'http://ec2-18-228-191-79.sa-east-1.compute.amazonaws.com:8080/api/iot/singin'
+
+print("Fill in the Vinos data!")
+
+email = raw_input('Email: ')
+password = raw_input('Password: ')
+
+payload = "{\n\t\"email\": \""+ email +"\",\n\"senha\": \""+ password +"\"\n}"
+headers = {'Content-Type': "application/json",'cache-control': "no-cache"}
+
+response = requests.request("POST", url_login, data = payload, headers = headers)
+
+devices = response.text
 
 #selecionar qual dispositivo sera usado para gerar a ulr
 
