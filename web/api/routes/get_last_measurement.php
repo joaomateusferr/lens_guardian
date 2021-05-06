20cda738471b611330a414fa4f598bd387021c4f
Joao Ferreira <joaomateusferr@gmail.com>
2021-05-06 13:22:18 -0300

    new file structure

--- web/api/routes/get_last_measurement.php
+++ web/api/routes/get_last_measurement.php
@@ -7,5 +7,5 @@
 		$Id = $_GET["id"];
 		$Device = new Device($Id);
 		echo json_encode($Device->getLastDayMeasurements());
-		
+		//to text -> http://localhost/lens_guardian/web/api/routes/get_last_measurement.php?id=1
 	}
